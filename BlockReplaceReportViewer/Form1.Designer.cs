761ab9ec8f75568cc5a42a86faf6d8aeed885ef8
Alex Fielder <alex.fielder@gmail.com>
2014-08-14 15:41:50 +0100

    Amended reportviewer to allow user to select .xml file to create report from.

--- BlockReplaceReportViewer/Form1.Designer.cs
+++ BlockReplaceReportViewer/Form1.Designer.cs
@@ -32,9 +32,17 @@
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.DrawingBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.Drawings = new BlockReplaceReportViewer.Drawings();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             ((System.ComponentModel.ISupportInitialize)(this.DrawingBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Drawings)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // DrawingBindingSource
@@ -48,6 +56,33 @@
             this.Drawings.Locale = new System.Globalization.CultureInfo("en-US");
             this.Drawings.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(559, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Generate Report";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(116, 7);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(437, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "XML File to Open:";
+            // 
             // reportViewer1
             // 
             this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -59,29 +94,59 @@
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "BlockReplaceReportViewer.Report1.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.Size = new System.Drawing.Size(284, 262);
-            this.reportViewer1.TabIndex = 0;
+            this.reportViewer1.Size = new System.Drawing.Size(1084, 682);
+            this.reportViewer1.TabIndex = 8;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.reportViewer1);
+            this.splitContainer1.Size = new System.Drawing.Size(1084, 717);
+            this.splitContainer1.SplitterDistance = 31;
+            this.splitContainer1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.reportViewer1);
+            this.ClientSize = new System.Drawing.Size(1084, 717);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.DrawingBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Drawings)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
         private Drawings Drawings;
         private System.Windows.Forms.BindingSource DrawingBindingSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
--- BlockReplaceReportViewer/Form1.cs
+++ BlockReplaceReportViewer/Form1.cs
@@ -22,14 +22,28 @@ namespace BlockReplaceReportViewer
         private void Form1_Load_1(object sender, EventArgs e)
         {
             string asmpath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            DirectoryInfo dir = new DirectoryInfo(asmpath + @"\temp");
+            FileInfo[] files = dir.GetFiles("*.xml");
+            foreach (FileInfo file in files)
+            {
+                comboBox1.Items.Add(file);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string asmpath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string exeFolder = Path.GetDirectoryName(Application.ExecutablePath);
             DataSet ds = new DataSet();
-            ds.ReadXml(asmpath + @"\temp\Drawings.xml");
-            ds.WriteXmlSchema(asmpath + @"\temp\Drawings.xsd");
+            ds.ReadXml(asmpath + "\\temp\\" + comboBox1.SelectedItem);
+            //ds.ReadXml(asmpath + @"\temp\Drawings.xml");
+            string schemaName = Path.GetFileNameWithoutExtension(comboBox1.SelectedItem.ToString()) + ".xsd";
+            ds.WriteXmlSchema(asmpath + "\\temp\\" + schemaName);
+            //ds.WriteXmlSchema(asmpath + @"\temp\Drawings.xsd");
             //ds = null;
-            DataSet snds = new DataSet();
-            snds.ReadXml(asmpath + @"\temp\Snapshots.xml");
-            snds.WriteXmlSchema(asmpath + @"\temp\Snapshots.xsd");
+            //DataSet snds = new DataSet();
+            //snds.ReadXml(asmpath + @"\temp\Snapshots.xml");
+            //snds.WriteXmlSchema(asmpath + @"\temp\Snapshots.xsd");
             DrawingBindingSource.DataSource = ds;
             this.reportViewer1.LocalReport.EnableExternalImages = true;
             ReportParameter AppPath = new ReportParameter("AppPath", exeFolder);
